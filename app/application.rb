47e870a438cf9dfd5e2d603951c927c7bfa1175d
Shujaat Azim <shujaat.azim@gmail.com>
2019-06-04 14:42:47 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
         if req.path.match(/items/)
 
             item_name = req.path.split("/items/").last
-            item = @@item.find {|item| item.name == item_name}  
+            item = @@items.find {|item| item.name == item_name}  
             if item 
                 resp.write item.price
             else
